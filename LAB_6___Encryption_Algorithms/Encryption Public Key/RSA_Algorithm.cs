ce00269c14d50bbf58c11e66b5ba94c7cf363542
KevinRomero3100 <59964519+KevinRomero3100@users.noreply.github.com>
2020-11-09 12:15:00 -0600

    InitialStructure

---
+++ LAB_6___Encryption_Algorithms/Encryption Public Key/RSA_Algorithm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_6___Encryption_Algorithms.Encryption_Public_Key
+{
+    class RSA_Algorithm : Interfaces.EncyptionPublicKey
+    {
+        public byte[] EncryptOrDecrypt(int n, int d, int e, byte[] bufer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetKey(int p, int q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LAB_6___Encryption_Algorithms/Interfaces/EncyptionPublicKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace LAB_6___Encryption_Algorithms.Interfaces
+{
+    interface EncyptionPublicKey
+    {
+        public void GetKey(int p, int q);
+        public byte[] EncryptOrDecrypt(int n, int d, int e, byte[] bufer);
+    }
+}
